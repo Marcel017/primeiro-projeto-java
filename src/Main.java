e9690e8bc5cb5b57425864dbb077393d0abdd4a2
Marcel017 <mcelogabriel17@gmail.com>
2025-12-10 22:01:40 -0300

    aprendendo como imprimir em text block (a partir do java 15)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Concluí a aula 01 e agora estou mergulhando em Java!");
-        }
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun Maveick");
+
+        int anoDeLançamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLançamento);
+        boolean incluidoNoPlano = true;
+        double noDoFilme = 8.1;
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLançamento;
+        System.out.println(sinopse);
+
+    }
 }
