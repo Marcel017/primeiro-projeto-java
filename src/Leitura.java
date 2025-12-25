f88c6d85633a192487d161c43fda3d1d359bb74f
Marcel017 <mcelogabriel17@gmail.com>
2025-12-24 23:57:58 -0300

    aqui estou vendo sobre importação e treinando um pouco mais sobre poo

--- src/Leitura.java
+++ src/Leitura.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Leitura {
     public static void main (String[] args) {
-        Scanner leitura = new Scanner()
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+        System.out.println(filme);
     }
 }
