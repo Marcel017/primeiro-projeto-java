71ad2c08ac21c78aac6ec313f7fcd5321da352e7
Marcel017 <mcelogabriel17@gmail.com>
2025-12-25 00:08:54 -0300

    entendendo um pouco mais sobre o Scanner

--- src/Leitura.java
+++ src/Leitura.java
@@ -6,6 +6,13 @@ public class Leitura {
 
         System.out.println("Digite seu filme favorito");
         String filme = leitura.nextLine();
+        System.out.println("Qual o ano de lançamento?");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println("Diga sua avaliação para o filme ");
+        double avaliacao = leitura.nextDouble();
+
         System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
     }
 }
