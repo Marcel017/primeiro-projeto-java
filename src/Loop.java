c224d1dd14d230ab294644b4d37c6526a05696ec
Marcel017 <mcelogabriel17@gmail.com>
2025-12-28 23:30:38 -0300

    entendendo um pouco mais de loop

---
+++ src/Loop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args){
+        Scanner leitura = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+
+        }
+
+        System.out.println("Diga sua avaliação para o filme ");
+        double avaliação = leitura.nextDouble();
+    }
+}
